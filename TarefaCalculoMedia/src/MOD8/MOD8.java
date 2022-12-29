6af6a530518b52199792edaf323c09d258adbe6b
guilherme-mmonteiro <guilherme.mont04@gmail.com>
2022-12-28 21:36:38 -0300

    mod8

---
+++ TarefaCalculoMedia/src/MOD8/MOD8.java
@@ -0,0 +1,21 @@
+package MOD8;
+
+public class MOD8 {
+
+	public static void main(String[] args) {
+		
+		 float primeiraNota = 8.5f;
+		
+		 float segundaNota = 6.0f;
+		
+		 float terceiraNota = 7.3f;
+		
+		 float quartaNota = 5.4f;
+		
+		String media = "a nota media é: " + ((primeiraNota + segundaNota + terceiraNota + quartaNota) / 4);
+		
+		System.out.println(media);
+		
+	}
+
+}
