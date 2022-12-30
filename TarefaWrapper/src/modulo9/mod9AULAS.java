fa3f507fb2eed55ad22210f8b242121a897acc44
guilherme-mmonteiro <guilherme.mont04@gmail.com>
2022-12-30 01:02:44 -0300

    modulo9

---
+++ TarefaWrapper/src/modulo9/mod9AULAS.java
@@ -0,0 +1,18 @@
+package modulo9;
+
+import java.util.*;
+
+public class mod9AULAS {
+	
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("digite um valor numerico");
+		int primitivo = sc.nextInt();
+			
+		Integer wrapper = primitivo;
+		
+		System.out.print("O valor wrapper é: " + wrapper);
+		
+	}
+}
